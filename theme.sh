03f727d90788ab5cba7b519b1ebd43d0c44499e0
35ch <67291226+35ch@users.noreply.github.com>
2022-11-10 09:40:05 +0100

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -1,34 +1,33 @@
-set $wallpaper ~/Downloads/bg.png
+set $wallpaper ~/Downloads/bg.jpg
 set $t #00000000
 
 font pango:Unifont 12
 
-default_border pixel 4
+default_border pixel 2
 
-# class                 border background text indicator
-client.focused          $t     $t         $t   $t        
-client.focused_inactive $t     $t         $t   $t         
-client.unfocused        $t     $t         $t   $t       
-client.urgent           $t     $t         $t   $t      
-client.placeholder      $t     $t         $t   $t       
-client.background       $t     $t         $t   $t        
+# class                 border  background text    indicator
+client.focused          #69a9c6 #69a9c6    #69a9c6 #69a9c6      
+client.focused_inactive #42484d #42484d    #42484d #42484d      
+client.unfocused        #303438 #303438    #303438 #303438   
+client.urgent           #cc6655 #cc6655    #cc6655 #cc6655 
+client.placeholder      $t      $t         $t      $t       
+client.background       $t      $t         $t      $t        
 
 output * bg $wallpaper fill
 
 bar {
-    position top
-	  gaps 4
+	position top
 
-	  colors {               
-	      statusline         $t
-		    background         #303438
+	colors {               
+		statusline         #dfe5eb
+		background         #303438
 
 #                          border  background text	
-		    active_workspace   $t      $t         #303438
-		    focused_workspace  $t      $t         #303438
-		    urgent_workspace   $t      $t         #303438
-		    inactive_workspace #303438 #303438    #dfe5eb
-		    }
+		active_workspace   #42484d #42484d    #dfe5eb
+		focused_workspace  #69a9c6 #69a9c6    #303438
+		urgent_workspace   #cc6655 #cc6655    #303438
+		inactive_workspace #303438 #303438    #dfe5eb
+		}
 																	   
-    status_command while ~/.config/sway/swaybar.sh; do sleep 1; done
+		status_command while ~/.config/sway/swaybar.sh; do sleep 1; done
 }
