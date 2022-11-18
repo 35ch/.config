29f6aa34a5571a8c07d8031655eb99e03b187f63
35ch <67291226+35ch@users.noreply.github.com>
2022-11-18 08:28:35 +0100

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -1,34 +1,37 @@
-set $wallpaper ~/Downloads/bg.png
-set $t #00000000
+set $wallpaper ~/wallpapers/macross.jpg
+
+set $t      #00000000
+set $bg     #363739cc
+set $fg     #babcbf
+set $lbg    #454649
+set $red    #c15a5a
+set $blu    #5893b6
 
 font pango:cozette bold 11
 
-
-default_border pixel 0
+default_border none
 
 # class                 border  background text    indicator
-client.focused          $t      $t         $t      $t    
-client.focused_inactive #333436 #333436    #333436 #333436     
-client.unfocused        $t      $t         $t      $t
-client.urgent           #853739 #853739    #853739 #853739
-client.placeholder      $t      $t         $t      $t       
-client.background       $t      $t         $t      $t        
+client.focused          $t      $blu       $blu    $blu 
+client.focused_inactive $t      $lbg       $lbg    $lbg  
+client.unfocused        $t      $bg        $bg     $bg  
+client.urgent           $t      $red       $red    $red  
 
 output * bg $wallpaper fill
 
 bar {
-	position top
+    position top
+    
+    colors {               
+	statusline         $fg
+	background         $bg
 
-	colors {               
-		statusline         #d0d7dd
-		background         #333436e4
-
-#                                  border    background text	
-		active_workspace   #333436e4 #333436e4  #d0d7dd
-		focused_workspace  #333436e4 #333436e4  #854f8c
-		urgent_workspace   #853739   #853739    #333436e4
-		inactive_workspace #333436e4 #333436e4  #d0d7dd
-		}
+#                          border    background text	
+	active_workspace   $bg       $bg        $fg
+	focused_workspace  $bg       $bg        $blu
+	urgent_workspace   $red      $red       $red
+	inactive_workspace $bg       $bg        $lbg
+	}
 																	  
-		status_command while ~/.config/sway/swaybar.sh; do sleep 1; done
+    status_command while ~/.config/sway/swaybar.sh; do sleep 1; done
 }
