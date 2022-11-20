2c46de5538d6eea59d7e23619eb263993b1967b9
35ch <67291226+35ch@users.noreply.github.com>
2022-11-20 17:02:09 +0100

    Simplified the script

--- swaybar.sh
+++ swaybar.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 date=$(date +"%Y-%m-%d")
 time=$(date +"%T")
 ram=$(free | grep Mem | awk '{printf "%.0f\n", $3/$2 *100}')
@@ -5,39 +7,19 @@ wifi_str=$(iw dev wlp5s0 station dump | grep signal: | awk '{print $2*-1}')
 inst=$(xbps-query -l | awk "END {print NR}")
 upd=$(xbps-install -Sun | awk "END {print NR}")
 
-if [ $wifi_str -le 12 ]
-then
-	wifi="▁▁▁▁▁▁▁▁"
+wifi_bars=("▁" "▂" "▃" "▄" "▅" "▆" "▇" "▉")
 
-elif [ $wifi_str -gt 12 -a $wifi_str -le 25 ]
-then
-	wifi="▁▁▁▁▁▁▁▂"
+# Half-up rounding wifi_str/12
+wifi_eight=$(( ($wifi_str + 12 / 2 ) / 12 ))
 
-elif [ $wifi_str -gt 25 -a $wifi_str -le 37 ]
-then
-	wifi="▁▁▁▁▁▁▂▃"
+wifi=${wifi_bars[$wifi_eight]}
 
-elif [ $wifi_str -gt 37 -a $wifi_str -le 50 ]
-then
-	wifi="▁▁▁▁▁▂▃▄"
-
-elif [ $wifi_str -gt 50 -a $wifi_str -le 62 ]
-then
-	wifi="▁▁▁▁▂▃▄▅"
-
-elif [ $wifi_str -gt 62 -a $wifi_str -le 75 ]
-then
-	wifi="▁▁▁▂▃▄▅▆"
-elif [ $wifi_str -gt 75 -a $wifi_str -le 87 ]
-then
-	wifi="▁▁▂▃▄▅▆▇"
-
-elif [ $wifi_str -gt 87 -a $wifi_str -le 100 ]
-then
-	wifi="▁▂▃▄▅▆▇▉"
-
-else
-	wifi="NO CONNECTION"
-fi	
+for i in {1..8}; do
+	if (($wifi_eight-$i < 0)); then
+		wifi+=${wifi_bars[0]}
+	else
+		wifi+=${wifi_bars[$wifi_eight-$i]}
+	fi
+done
 
 echo "( XBPS: $inst installed, $upd updates ( RAM: $ram% ( $time ( $date ) $wifi "
