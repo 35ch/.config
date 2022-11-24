2c526860372462c5c87c5649f33ddc41593b8acb
35ch <67291226+35ch@users.noreply.github.com>
2022-11-24 15:56:11 +0100

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -1,21 +1,19 @@
-set $wallpaper ~/wallpapers/macross.jpg
-
 set $t      #00000000
-set $bg     #363739cc
+set $bg     #363739
 set $fg     #babcbf
 set $lbg    #454649
 set $red    #c15a5a
 set $blu    #5893b6
 
-font pango:cozette bold 11
+font pango:Cozette bold 11
 
-default_border none
+default_border pixel 6
 
 # class                 border  background text    indicator
-client.focused          $t      $blu       $blu    $blu 
-client.focused_inactive $t      $lbg       $lbg    $lbg  
-client.unfocused        $t      $bg        $bg     $bg  
-client.urgent           $t      $red       $red    $red  
+client.focused          $t      $t         $t      $t
+client.focused_inactive $t      $t         $t      $t
+client.unfocused        $t      $t         $t      $t
+client.urgent           $t      $t         $t      $t
 
 output * bg $wallpaper fill
 
@@ -30,7 +28,7 @@ bar {
 	active_workspace   $bg       $bg        $fg
 	focused_workspace  $bg       $bg        $blu
 	urgent_workspace   $red      $red       $red
-	inactive_workspace $bg       $bg        $lbg
+	inactive_workspace $bg       $bg        $fg
 	}
 																	  
     status_command while ~/.config/sway/swaybar.sh; do sleep 1; done
