daf4035f463c4ead7ea6f5af3c7868bf68661923
35ch <67291226+35ch@users.noreply.github.com>
2022-12-06 22:21:11 +0100

    Update swaybar.sh

--- swaybar.sh
+++ swaybar.sh
@@ -1,52 +1,70 @@
 #!/bin/bash
 
-function year_and_date() {
-	echo $(date +"%Y-%m-%d")
+write () {
+	echo "$1 |" 
 }
 
-function current_time() {
-	echo $(date +"%H:%M")
+year_and_date() {
+	write "$(date +"%Y-%m-%d")"
 }
 
-function ram_usage() {
-	echo $(free -b | head -n 2 | tail -1 | awk '{printf "%.0f\n", $3/$2 * 100}')
+current_time() {
+	write "$(date +"%H:%M:%S")"
 }
 
-function updates_available() {
-	echo $(xbps-install -Mun | awk "END {print NR}")
+ram_usage() {
+	write "RAM: $(free -b | head -n 2 | tail -1 | awk '{printf "%.0f\n", $3/$2 * 100}')%"
 }
 
-function local_ip_adress() {
-	echo $(ip route get 1 | awk '{print $7}')
+packages_installed() {
+	write $(xbps-query -l | awk "END {print NR}")
 }
 
-function disk_usage() {
-	echo $(df -h --total | tail -1 | awk '{print $5}')
+updates_available() { 
+	write "XBPS: $(xbps-install -Mun | awk "END {print NR}") updates"
 }
 
-function cpu_temperature() {
-	echo $(cat /sys/devices/platform/eeepc-wmi/hwmon/hwmon3/subsystem/hwmon2/temp1_input | awk '{printf "%.0f\n", $1/1000}')
+local_ip_adress() {
+	write "$(wpa_cli status | tail -5 | head -n 1 | cut -c 12-)"
 }
 
-function wifi_bars() {
+cpu_temperature() {
+	write "CPU: $(cat /sys/devices/platform/eeepc-wmi/hwmon/hwmon3/subsystem/hwmon2/temp1_input | awk '{printf "%.0f\n", $1/1000}')°C"
+} 
+
+wifi() {
+	if [[ $(wpa_cli ping | tail -1) == "PONG" ]] & [[ $(dhcpcd -U | grep ip | cut -c 12-) != "" ]]; then
+		local wifi=""
+	else
+		local wifi="睊"
+	fi
+
+	write "$wifi"
+}
+
+wifi_bars() {
 	wifi_signal=$(iw dev wlp5s0 station dump | grep signal: | awk '{print $2*-1}')
 	
-	bars=("▁" "▂" "▃" "▄" "▅" "▆" "▇" "▉")
+	if [[ $wifi_signal == "" ]]; then
+		local wifi="Not connected"
+	else
+		bars=("▁" "▂" "▃" "▄" "▅" "▆" "▇" "▉")
 
-	# Half-up rounding wifi_str/12
-	wifi_eight=$(( ($wifi_signal + 12 / 2 ) / 12 ))
+		# Half-up rounding wifi_str/12
+		wifi_eight=$(( ($wifi_signal + 12 / 2 ) / 12 ))
 
-	wifi=${bars[$wifi_eight]}
+		wifi=${bars[$wifi_eight]}
 	
-	for i in {1..8}; do
-		if (($wifi_eight-$i < 0)); then
-			wifi+=${bars[0]}
-		else
-			wifi+=${bars[$wifi_eight-$i]}
-		fi
-	done
-	
-	echo $wifi
+		for i in {1..8}; do
+			if (($wifi_eight-$i < 0)); then
+				wifi+=${bars[0]}
+			else
+				wifi+=${bars[$wifi_eight-$i]}
+			fi
+		done
+	fi
+
+	write $wifi
 }
 
-echo "$(local_ip_adress) | $(wifi_bars) | XBPS: $(updates_available) updates | RAM: $(ram_usage)% | CPU: $(cpu_temperature)°C | DISK: $(disk_usage) | $(current_time) | $(year_and_date) "
+echo "$(local_ip_adress) $(updates_available) $(ram_usage) $(cpu_temperature) $(year_and_date) $(current_time) $(wifi)"
