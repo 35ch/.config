ea841338789328cfda28290ebcca0dbc82a986d9
35ch <67291226+35ch@users.noreply.github.com>
2022-12-07 07:47:08 +0100

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -1,35 +1,34 @@
 set $t      #00000000
 set $bg     #363739
 set $fg     #babcbf
-set $lbg    #454649
-set $red    #c15a5a
-set $blu    #5893b6
+set $red    #ca5a5a
 
-font pango:Cozette bold 11
+font pango:Cozette Bold 11
 
 default_border pixel 6
 
 # class                 border  background text    indicator
-client.focused          $t      $t         $t      $t
-client.focused_inactive $t      $t         $t      $t
-client.unfocused        $t      $t         $t      $t
-client.urgent           $t      $t         $t      $t
+client.focused          $t      $t         $t      $t 
+client.focused_inactive $t      $t         $t      $t  
+client.unfocused        $t      $t         $t      $t  
+client.urgent           $t      $t         $t      $t  
 
-output * bg $wallpaper fill
 
 bar {
-    position top
+	position top
     
-    colors {               
-	statusline         $fg
-	background         $bg
+	gaps 6
+
+	colors {               
+		statusline         $fg
+		background         $bg
 
 #                          border    background text	
-	active_workspace   $bg       $bg        $fg
-	focused_workspace  $bg       $bg        $blu
-	urgent_workspace   $red      $red       $red
-	inactive_workspace $bg       $bg        $fg
-	}
-																	  
-    status_command while ~/.config/sway/swaybar.sh; do sleep 1; done
+		active_workspace   $bg       $bg        $t
+		focused_workspace  $bg       $bg        $fg
+		urgent_workspace   $red      $red       $red
+		inactive_workspace $bg       $bg        $t
+		}
+	
+		status_command while ~/.config/sway/swaybar.sh; do sleep 1; done
 }
